de6a3d665c3b67cff64b2f3455a217fc2d9a8015
Lutkenhouse <Holly.A.Lutkenhouse@disney.com>
2018-01-25 14:56:45 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
-console.log("Up and running!");
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+//if statement that checks to see if the length of the cardsInPlay array is 2
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
