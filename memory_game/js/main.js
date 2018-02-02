7a5dd21e35ec679c7efa318153370438bae8a3c6
Lutkenhouse <Holly.A.Lutkenhouse@disney.com>
2018-02-02 05:21:39 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,16 +9,37 @@ console.log("User flipped " + cardThree);
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-//if statement that checks to see if the length of the cardsInPlay array is 2
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+
+var checkForMatch = function (){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
     alert("You found a match!");
-  }
-  else {
+} else {
+    console.log("Sorry, try again!");
     alert("Sorry, try again.");
-  }
+}
+};
+
+checkForMatch (0,2);
+/*  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again.");
+    }
+}*/
+
+var flipCard = function (cardId){
+  /*var cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log("User flipped " + cardOne);
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  console.log("User flipped " + cardTwo);
+  */
+  //flipCard (0);
+  //flipCard (2);
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch ();
+}
