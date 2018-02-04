7a0ef42e5b044866ef1d2915390cc5cdd400f4f3
Lutkenhouse <Holly.A.Lutkenhouse@disney.com>
2018-02-04 05:23:55 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,8 +6,30 @@ var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 */
+//var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: 'queen',
+  suit: 'hearts',
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: 'queen',
+  suit: 'diamonds',
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: 'king',
+  suit: 'hearts',
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: 'king',
+  suit: 'diamonds',
+  cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var checkForMatch = function (){
@@ -20,7 +42,7 @@ var checkForMatch = function (){
 }
 };
 
-checkForMatch (0,2);
+//checkForMatch (0,2);
 /*  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match!");
     }
@@ -37,9 +59,12 @@ var flipCard = function (cardId){
   cardsInPlay.push(cardTwo);
   console.log("User flipped " + cardTwo);
   */
-  //flipCard (0);
-  //flipCard (2);
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-  checkForMatch ();
-}
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+};
+
+flipCard (0);
+flipCard (2);
+checkForMatch ();
